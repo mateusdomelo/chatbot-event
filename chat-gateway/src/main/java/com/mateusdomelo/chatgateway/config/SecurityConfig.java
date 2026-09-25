5cab21e6f9cfce104e3bb3829b3ea5bc30f99a66
Mateus Melo <mateusdomelo@gmail.com>
2026-09-25 14:29:40 -0300

    feat: adicionar camada de autenticação e configurar actuator

---
+++ chat-gateway/src/main/java/com/mateusdomelo/chatgateway/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.mateusdomelo.chatgateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(
+            PasswordEncoder passwordEncoder,
+            @Value("${app.security.username}") String username,
+            @Value("${app.security.password}") String password) {
+
+        UserDetails user = User.withUsername(username)
+                .password(passwordEncoder.encode(password))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/chat")
+                        .hasRole("USER")
+                        .anyRequest().authenticated())
+                .httpBasic(httpBasic -> {})
+                .build();
+    }
+}
